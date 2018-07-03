4c515e9ea245cd9f1a9f432e05ce903d2cb0e8d7
LOUIS Z J LI (ITA-ISDC-ISD-OOCLL/ZHA) <LILO4@OOCL.COM>
2018-07-03 22:43:02 +0800

    [louis] section-3-practice3

--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,5 +1,40 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+	Array.prototype.unique = function(){
+	  var res = [];
+	  var json = {};
+	  for(var i = 0; i < this.length; i++){
+		if(!json[this[i]]){
+		 res.push(this[i]);
+		 json[this[i]] = 1;
+		}
+	  }
+	  return res;
+	}
+	var collectionAunique = collectionA.unique();
+	var collectionC = [];
+	for(var i = 0;i < collectionAunique.length;i++){
+		var obj = {}
+		obj.key = collectionAunique[i];
+		obj.count = 0;
+		for(var j in collectionA){
+			if(collectionAunique[i] === collectionA[j]){
+				obj.count++;
+			}
+		}
+		collectionC.push(obj);
+	}
+	for(var i in collectionC){
+	  for(var j in objectB.value){
+		  if(collectionC[i].key === objectB.value[j]){
+			var num = parseInt(collectionC[i].count/3);
+			if (num > 0) {
+			  collectionC[i].count = collectionC[i].count - num;
+			}
+		  }
+	  }
+	}
+  console.log(collectionC)
+  return collectionC;
 }
