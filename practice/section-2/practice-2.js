0e5fde0f4f89680f1b5580158d2a5f4008c88efe
LOUIS Z J LI (ITA-ISDC-ISD-OOCLL/ZHA) <LILO4@OOCL.COM>
2018-07-03 21:11:24 +0800

    [louis] section-2-practice2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,37 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+	Array.prototype.unique = function(){
+	  var res = [];
+	  var json = {};
+	  for(var i = 0; i < this.length; i++){
+		if(!json[this[i]]){
+		  if(this[i].indexOf('-') > 0){
+			res.push(this[i].substring(0,1));
+		  } else {
+		    res.push(this[i]);
+		  }
+		json[this[i]] = 1;
+		}
+	  }
+	  return res;
+	  }
+	  var arr = collection.unique();
+	  var result = [];
+	  for(var i = 0; i < arr.length; i++) {
+		  var obj = {};
+		  obj.key = arr[i];
+		  obj.count = 0;
+		  for (var j = 0; j < collection.length; j++){
+			if(arr[i] == collection[j]){
+				obj.count++;
+			}
+			if(arr[i] == collection[j].substring(0,1) && arr[i] != collection[j]){
+				obj.count = parseInt(collection[j].substring(2));
+				break;
+			}
+		  }
+		  result.push(obj)
+	  }
+	  return result;
 }
