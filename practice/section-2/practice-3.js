d694c201ece836d5b9c99c7d7e6ae9c329b59834
LOUIS Z J LI (ITA-ISDC-ISD-OOCLL/ZHA) <LILO4@OOCL.COM>
2018-07-03 22:01:47 +0800

    [louis] section-2-practice3

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,39 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  	Array.prototype.unique = function(){
+	  var res = [];
+	  var json = {};
+	  for(var i = 0; i < this.length; i++){
+		if(!json[this[i]] && !json[this[i].substring(0,1)]){
+		  if(this[i].indexOf('-') > 0 || this[i].indexOf(':') > 0 || this[i].indexOf('[') > 0){
+			res.push(this[i].substring(0,1));
+			json[this[i].substring(0,1)] = 1;
+		  } else {
+		    res.push(this[i]);
+			json[this[i]] = 1;
+		  }
+		
+		}
+	  }
+	  return res;
+	  }
+	  var arr = collection.unique();
+	  var result = [];
+	  for(var i = 0; i < arr.length; i++) {
+		  var obj = {};
+		  obj.name = arr[i];
+		  obj.summary = 0;
+		  for (var j = 0; j < collection.length; j++){
+			if(arr[i] == collection[j]){
+				obj.summary++;
+			}
+			if(arr[i] == collection[j].substring(0,1) && arr[i] != collection[j]){
+				var num = collection[j].replace(/[^0-9]/ig,"");
+				obj.summary = obj.summary + parseInt(num);
+			}
+		  }
+		  result.push(obj)
+	  }
+	  return result;
 }
